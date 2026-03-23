ed33385fb39aeccb92ad6b6dc27e8a35b5e6c6ca
zaid99 <150908673+zaid1243@users.noreply.github.com>
2026-03-23 18:03:10 +0530

    Refactor App component structure with sections

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,29 @@ const App = () => {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>{" "}
       </div>
       <div className="container mx-auto px-8">
-        <Navbar />
-        <Hero />
-        <Projects />
-        <About />
-        <Technologies />
-        <Education />
-        <Contact />
+        <div id="home">
+  <Hero />
+</div>
+
+<div id="projects">
+  <Projects />
+</div>
+
+<div id="about">
+  <About />
+</div>
+
+<div id="technologies">
+  <Technologies />
+</div>
+
+<div id="education">
+  <Education />
+</div>
+
+<div id="contact">
+  <Contact />
+</div>
       </div>
     </div>
   );

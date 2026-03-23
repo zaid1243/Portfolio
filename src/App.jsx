c3b193a081490089a21546fb074bca7b3676e53f
zaid <skzaid8821@email.com>
2026-03-23 20:58:07 +0530

    gt

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
 
+      <Navbar />
       <div className="container mx-auto px-8 relative z-10">
-        <Navbar />
         {/* ✅ IMPORTANT: IDs + scroll offset */}
         <div id="home" className="scroll-mt-24">
           <Hero />

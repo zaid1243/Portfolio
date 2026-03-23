f040c56703c7ba1d7403b9f5ae09ab2877ad8e19
zaid99 <150908673+zaid1243@users.noreply.github.com>
2026-03-23 18:22:40 +0530

    Include Navbar component in App
    Added Navbar component to the main App component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Education from "./components/Education";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Technologies from "./components/Technologies";
+import Technologies from "./components/Technologies";import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>{" "}
       </div>
       <div className="container mx-auto px-8">
+        <Navbar />   {/* 👈 ADD THIS */}
         <div id="home">
   <Hero />
 </div>

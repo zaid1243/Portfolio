80ab39caad6fa1037ed0eff7af452d0af3af33b8
zaid99 <150908673+zaid1243@users.noreply.github.com>
2026-03-23 18:18:47 +0530

    Simplify scroll to section functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,7 @@ const Hero = () => {
 const scrollToSection = (id) => {
   const element = document.getElementById(id);
   if (element) {
-    const yOffset = -80;
-    const y =
-      element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-
-    window.scrollTo({ top: y, behavior: "smooth" });
+    element.scrollIntoView({ behavior: "smooth" });
   }
 };
   return (

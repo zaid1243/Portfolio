facfdb404b5fab1811d07b6b7bb7dd0b02137ecf
zaid <skzaid8821@email.com>
2026-03-23 20:38:49 +0530

    asa

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <div className="text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       {/* BACKGROUND FIX */}
+      {/* <div className="fixed top-0 -z-10 h-full w-full pointer-events-none"> */}
       <div className="fixed top-0 -z-10 h-full w-full pointer-events-none">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
